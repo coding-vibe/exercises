ad61abd02c2488a83f49411cfd9fb552e5aabd1c
merry <k.maria.m.24.05@gmail.com>
2023-08-02 14:20:21 +0300

    simplify calculator

--- calculator.js
+++ calculator.js
@@ -1,95 +1,63 @@
 interface ArithmeticOperations {
-    value1: number;
-    value2?: number;
-    toAdd: (value1: number, value2?: number) => this;
-    toSubtract: (value1: number, value2?: number) => this;
-    toMultiply: (value1: number, value2?: number) => this;
-    toDivide: (value1: number, value2?: number) => this;
+  value: number;
+  toAdd: (value: number) => this;
+  toSubtract: (value: number) => this;
+  toMultiply: (value: number) => this;
+  toDivide: (value: number) => this;
 }
 
 interface AdvancedOperations {
-    valueToPow?: number;
-    power: number;
-    toSquare: (valueToPow?: number) => this;
-    toCube: (valueToPow?: number) => this;
-    toPower: (power: number, valueToPow?: number) => this;
+  power: number;
+  toSquare: () => this;
+  toCube: () => this;
+  toPower: (power: number) => this;
 }
 
 class Calculator implements ArithmeticOperations, AdvancedOperations {
-  value1: number;
-  value2: number;
-  valueToPow: number;
+  value: number;
+
   power: number;
+
   currentResult: number;
 
-  constructor(value1 = 0, value2 = 0, valueToPow = 0, power = 1) {
-    this.value1 = value1;
-    this.value2 = value2;
-    this.valueToPow = valueToPow;
+  constructor(value = 0, power = 1) {
+    this.value = value;
     this.power = power;
-    this.currentResult = 0;
+    this.currentResult = value;
   }
 
-  toAdd(value1: number, value2?: number): this {
-    if (value2) {
-      this.currentResult = value1 + value2;
-    } else {
-      this.currentResult += value1;
-    }
+  toAdd(value: number): this {
+    this.currentResult += value;
     return this;
   }
 
-  toSubtract(value1: number, value2?: number): this {
-    if (value2) {
-      this.currentResult = value1 - value2;
-    } else {
-      this.currentResult -= value1;
-    }
+  toSubtract(value: number): this {
+    this.currentResult -= value;
     return this;
   }
 
-  toMultiply(value1: number, value2?: number): this {
-    if (value2) {
-      this.currentResult = value1 * value2;
-    } else {
-      this.currentResult *= value1;
-    }
+  toMultiply(value: number): this {
+    this.currentResult *= value;
     return this;
   }
 
-  toDivide(value1: number, value2?: number): this {
-    if (value2) {
-      this.currentResult = value1 / value2;
-    } else {
-      this.currentResult /= value1;
-    }
+  toDivide(value: number): this {
+    this.currentResult /= value;
     return this;
   }
 
-  toSquare(valueToPow?: number, power = 2): this {
-    if (valueToPow) {
-      this.currentResult = valueToPow ** power;
-    } else {
-      this.currentResult **= power;
-    }
+  toSquare(power = 2): this {
+    this.currentResult **= power;
     return this;
   }
 
-  toCube(valueToPow?: number, power = 3): this {
-    if (valueToPow) {
-      this.currentResult = valueToPow ** power;
-    } else {
-      this.currentResult **= power;
-    }
+  toCube(power = 3): this {
+    this.currentResult **= power;
     return this;
   }
 
-  toPower(power: number, valueToPow?: number): this {
-    if (valueToPow) {
-      this.currentResult = valueToPow ** power;
-    } else {
-      this.currentResult **= power;
-    }
+  toPower(power: number): this {
+    this.currentResult **= power;
     return this;
   }
 
@@ -98,14 +66,14 @@ class Calculator implements ArithmeticOperations, AdvancedOperations {
   }
 }
 
-const myCalculator = new Calculator();
+const myCalculator = new Calculator(1);
 
 console.log(myCalculator
-  .toAdd(10, 10)
+  .toAdd(20)
   .toSubtract(10)
   .toMultiply(10)
   .toDivide(5)
   .toSquare()
   .toCube()
-  .toPower(11)
+  .toPower(10)
   .getResult());
