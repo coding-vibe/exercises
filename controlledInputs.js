834528e781edac44343d9c739cd00d62f93b0627
merry <k.maria.m.24.05@gmail.com>
2023-08-30 02:28:57 +0300

    Create controlledInputs.js

---
+++ controlledInputs.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+
+function controlledInputs() {
+    const [checked, setChecked] = useState(false);
+    const [option, setOption] = useState('Not done');
+    const handleCheckboxChange = () => {
+        setChecked(!checked);
+    };
+    const handleRadioChange = (event) => {
+        setOption(event.target.value);
+    };
+
+    return (
+        <>
+            <p>Controlled checkbox</p>
+            <label htmlFor='accept'>
+                Accept
+                <input
+                    type='checkbox'
+                    name='checkbox_name'
+                    value='checked'
+                    id='accept'
+                    checked={checked}
+                    onChange={handleCheckboxChange}
+                />
+            </label>
+            <p>Controlled radio</p>
+            <label htmlFor='done'>
+                <input
+                    type='radio'
+                    name='status'
+                    value='done'
+                    id='done'
+                    checked={option === 'Done'}
+                    onChange={handleRadioChange}
+                />
+                Done
+            </label>
+            <label htmlFor='not done'>
+                <input
+                    type='radio'
+                    name='status'
+                    value='not done'
+                    id='not done'
+                    checked={option === 'Not done'}
+                    onChange={handleRadioChange}
+                />
+                Not done
+            </label>
+        </>
+    );
+};
