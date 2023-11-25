842e3e452403c27cc5347116f4919fb826219992
merry <k.maria.m.24.05@gmail.com>
2023-11-25 21:41:28 +0200

    update name

--- usePrevious.js
+++ usePrevious.js
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react';
 
-const previousStateTracker = (value) => {
+const usePrevious = (value) => {
     const previousValue = useRef();
 
     useEffect(() => {
